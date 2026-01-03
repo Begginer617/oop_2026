e17d6678ecaf6b6dd3f7ef6ccd05f5c1a22d1313
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-01-03 11:29:29 +0100

    oop

--- Czlowiek.py
+++ Czlowiek.py
@@ -1,20 +1,24 @@
-# powstanie klasy = szablon, przepis
-class czlowiek:
-    gatunek = "homo sapiens"
+# Klasa = Szablon, Przepis
+class Czlowiek:
+# Istota
+gatunek = "Homo Sapiens"
+    def __init__(self):
+    def __init__(self, imie):
+# Konstruktor
+# Akt Istnienia
+        print("Niech powstanie Czlowiek")
+        print(f"Niech powstanie Czlowiek o imieniu {imie}")
+        self.imie = imie
+        # adam.imie = "Adam"
+        # ewa.imie = "Ewa"
 
-#inicjalizacja obiektu, konstruktor
-def __init__(self):
-    print("niech powstanie human")
-
-
-
-#powstanie obiektu
-# gotowanie z przepisu
-
-
-# print(dir(czlowiek))
-#
-adam = czlowiek()
-print(adam)
+# Powstawanie obiektu
+# Gotowanie z przepisu
+adam = Czlowiek() # a = 4 # a = int(4)
 print(adam.gatunek)
-
+adam = Czlowiek("Adam") # a = 4 # a = int(4)
+ewa = Czlowiek("Ewa")
+print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
