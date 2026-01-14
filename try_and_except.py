7e3c3af51ea6ed5ffd25e7a4154fcde454304224
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-01-14 17:11:12 +0100

    message

--- try_and_except.py
+++ try_and_except.py
@@ -25,9 +25,10 @@ while (True):
 
 #
 # Można obsługiwać konkretne typy wyjątków
-# :except ValueError:
-# except ZeroDivisionError:
-# except FileNotFoundError:
+# :except ValueError: zla podana wartpsc int zamiast str i tym podobne
+# except ZeroDivisionError: do dzilenia przez 0
+# except FileNotFoundError: do polikow
+#   except IndexError: do tablic , slownikow
 #
 # Można też obsłużyć kilka typów naraz:
 # except (TypeError, ValueError):
