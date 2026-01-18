da83632e4ee3b38e11b01903bdb97c12659eb896
Begginer617 <skorupskimilosz@gmail.com>
2026-01-19 00:21:16 +0100

    opp

--- Ciasto.py
+++ Ciasto.py
@@ -1,26 +1,29 @@
 class Ciasto:
-    # WSPÓLNA baza biszkoptu dla wszystkich ciast
     BAZA_BISZKOPTU = ["mąka", "jajka", "cukier"]
 
-    def __init__(self, nazwa_ciasta, skladniki=None):
+    def __init__(self, nazwa_ciasta):
         self.nazwa_ciasta = nazwa_ciasta
-        if skladniki: # jeśli nie podam składników → używa bazy biszkoptu
-            self.skladniki = list(skladniki)
-        else:
-            self.skladniki = list(self.BAZA_BISZKOPTU)
+        self.skladniki = self.BAZA_BISZKOPTU.copy()
 
     def dodaj_skladnik(self, skladnik):
         self.skladniki.append(skladnik)
+
+    def kopiuj(self):
+        nowe = Ciasto(self.nazwa_ciasta)
+        nowe.skladniki = self.skladniki.copy()
+        return nowe
 
     def __str__(self):
         return f"Ciasto: {self.nazwa_ciasta}, składniki: {', '.join(self.skladniki)}"
 
 
-ciasto_czekoladowe = Ciasto("Czekoladowe")
-ciasto_czekoladowe.dodaj_skladnik("kakao")
+# ⬇️ TO MUSI BYĆ POZA KLASĄ
+ciasto1 = Ciasto("Czekoladowe")
+ciasto1.dodaj_skladnik("kakao")
+ciasto1.dodaj_skladnik("czekolada")
 
-ciasto_truskawkowe = Ciasto("Truskawkowe")
-ciasto_truskawkowe.dodaj_skladnik("truskawki")
+ciasto2 = ciasto1.kopiuj()
+ciasto2.dodaj_skladnik("orzechy")
 
-print(ciasto_czekoladowe)
-print(ciasto_truskawkowe)
+print(ciasto1)
+print(ciasto2)
--- pracownik.py
+++ pracownik.py
@@ -1,24 +1,40 @@
-#   Program do zarządzania małą firmą
-
-# Bez programowania obiektowego:
-pracownik1_imie = "Adam"
-pracownik1_nazwisko = "Nowak"
-pracownik1_pensja = 4500
-
-pracownik2_imie = "Anna"
-pracownik2_nazwisko = "Nowak"
-pracownik2_pensja = 4500
-
-pracownik2_pensja = pracownik2_pensja + 100
-
-## OOP
-class Pracownik:
-    def __init__(self, imie, nazwisko,pensja):
+class Zarzad:
+    def __init__(self, imie, nazwisko, pensja, ranga_dyrektora):
         self.imie = imie
         self.nazwisko = nazwisko
         self.pensja = pensja
-    def daj_podwyzje(self, kwota):
-        self.pensja = self.pensja + kwota
+        self.ranga_dyrektora = ranga_dyrektora
 
+    def zwolnij_pracownika(self, pracownik):
+        print(f"Pracownik {pracownik.imie} {pracownik.nazwisko} został zwolniony.")
+
+    def zatrudnij_pracownika(self, pracownik):
+        print(f"Pracownik {pracownik.imie} {pracownik.nazwisko} został zatrudniony.")
+
+
+class Manager(Zarzad):
+    def __init__(self, imie, nazwisko, pensja, ranga_managera):
+        super().__init__(imie, nazwisko, pensja, ranga_managera)
+
+
+class Pracownik:
+    def __init__(self, imie, nazwisko, pensja):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.pensja = pensja
+
+    def daj_podwyzke(self, kwota):
+        self.pensja += kwota
+        print(f"{self.imie} {self.nazwisko} otrzymał podwyżkę o {kwota} zł. Nowa pensja: {self.pensja} zł.")
+
+
+# Tworzenie obiektów
 adam_nowak = Pracownik("Adam", "Nowak", 4500)
-adam_nowak.daj_podwyzje(100)
+adam_nowak.daj_podwyzke(100)
+anna_nowak = Pracownik("Anna", "Nowak", 4500)
+ziomek = Manager("Jan", "Kowalski", 8000, "Manager")
+ziomek.zwolnij_pracownika(anna_nowak)
+
+# Manager zatrudnia i zwalnia pracowników
+ziomek.zatrudnij_pracownika(anna_nowak)
+ziomek.zwolnij_pracownika(adam_nowak)
